b053ff9a23c9a5e3c7dec7beb24e34ad6536d8e8
Pedro <pedro@eletrocenterlondrina.com.br>
2025-03-14 16:35:08 -0300

    first commit

---
+++ backend/coralcobragames-infrastructure/coralcobragames-web/src/main/java/br/com/coralcobragames/infrastructure/domain/WebApplication.java
@@ -0,0 +1,7 @@
+package br.com.coralcobragames.infrastructure.domain;
+
+public class WebApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
