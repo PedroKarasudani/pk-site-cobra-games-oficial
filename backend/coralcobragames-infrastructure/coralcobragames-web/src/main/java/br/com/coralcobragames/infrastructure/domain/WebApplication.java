5323439d0cf5496948ffcefb5b80f330918e9e7f
Pedro Karasudani Desktop <pedro.karasudani@gmail.com>
2025-05-26 23:51:22 -0300

    restructuring backend

--- backend/coralcobragames-infrastructure/coralcobragames-web/src/main/java/br/com/coralcobragames/infrastructure/domain/WebApplication.java
+++
@@ -1,7 +0,0 @@
-package br.com.coralcobragames.infrastructure.domain;
-
-public class WebApplication {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
-}
