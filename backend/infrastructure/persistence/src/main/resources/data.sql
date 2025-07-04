b871ce1f48e04f203660c0de4f3d3f206efaada3
Pedro Karasudani Desktop <pedro.karasudani@gmail.com>
2025-07-03 23:42:28 -0300

    data.sql atualizado

--- backend/infrastructure/persistence/src/main/resources/data.sql
+++ backend/infrastructure/persistence/src/main/resources/data.sql
@@ -1,2 +1,5 @@
-INSERT INTO tb_users (name, email, password, role, enabled) VALUES ('Pedro','pedro@mail.com','123','ADMIN',true);
-INSERT INTO tb_contact_message (name, email, message, created_at) VALUES ('Pedro','pedro@mail.com','Olá Amigo', CURRENT_TIMESTAMP());
+INSERT INTO tb_users (name, email, password, role, enabled) VALUES ('Jose','jose@mail.com','123','ADMIN',true);
+INSERT INTO tb_contact_message (name, email, message, created_at) VALUES ('Barbara','barbara@mail.com','Hello word', CURRENT_TIMESTAMP);
+INSERT INTO tb_team_member (id, name, photo_url, role, linkedin_url) VALUES (1, 'Pedro', 'www.photo.com.br', 'Programador', 'www.linkedinUrl.com.br');
+
+INSERT INTO tb_update (title, full_text, date, type, author_id) VALUES ('Nova funcionalidade: Login com Google', 'Agora é possível logar com a conta do Google.', CURRENT_TIMESTAMP,'NOVA_FUNCIONALIDADE', 1);
